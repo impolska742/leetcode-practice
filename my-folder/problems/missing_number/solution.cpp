7abc77ea031fd1272b3008c654bf58a35862b727
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-09-07 14:49:04 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- my-folder/problems/missing_number/solution.cpp
+++ my-folder/problems/missing_number/solution.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int res = 0;
-        int n = nums.size();
-        res = n*(n+1)/2;
-        for(auto it : nums) res -= it;
-        return res;
+    int missingNumber(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0;
+        while(i < n) {
+            if(arr[i] < n and arr[i] != arr[arr[i]]) {
+                swap(arr[i], arr[arr[i]]);
+            } else {
+                i++;
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(arr[i] != i) return i;
+        }
+        
+        return n;
     }
 };
