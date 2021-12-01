6cc741fb79501bde4316d74627eb910dae2d5e96
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-12-01 13:30:05 +0000

    Sync LeetCode submission - Sum of Unique Elements (cpp)

---
+++ my-folder/problems/sum_of_unique_elements/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum = 0;
+        unordered_map<int, int> mp;
+        for(auto it : nums) {
+            mp[it]++;
+        }
+        for(auto it : mp) {
+            if(it.second == 1) {
+                sum += it.first;
+            }
+        }
+        return sum;
+    }
+};
