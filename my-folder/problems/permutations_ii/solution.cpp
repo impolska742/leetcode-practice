95d7f36e39e110c1f167450e8fd678fcdd23fdd5
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2022-01-29 15:26:32 +0000

    Sync LeetCode submission - Permutations II (cpp)

---
+++ my-folder/problems/permutations_ii/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+     void help(vector<int>& nums, int idx, map<int, int> &mapp, vector<vector<int>> &ans, vector<int> &temp) {
+        int n = nums.size();
+        if(idx == n) {
+            if(temp.size() == n) {
+                ans.push_back(temp);
+            }
+            return;
+        }
+        
+        for(auto &it : mapp) {
+            if(it.second > 0) {
+                it.second--;
+                temp.push_back(it.first);
+                help(nums, idx + 1, mapp, ans, temp);
+                temp.pop_back();
+                it.second++;
+            }    
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        map<int, int> mapp;
+        for(auto item : nums)
+            mapp[item]++;
+        vector<int> temp;
+        help(nums, 0, mapp, ans, temp);
+        return ans;
+    }
+    
+};
