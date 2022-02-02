ea5e6a1899eec739b1f7693aa7ba9c0741fdcf05
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2022-02-02 14:24:03 +0000

    Sync LeetCode submission - N-Queens II (cpp)

---
+++ my-folder/problems/n-queens_ii/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int count = 0;
+    vector<bool> ld, rd, col;
+    
+    void solve(int n, vector<string> &board, int r) {
+        if(r == n) {
+            count++;
+            return;
+        }
+        
+        for(int c = 0; c < n; c++) {
+            if(!col[c] and !ld[r + c] and !rd[r - c + n - 1]) {
+                board[r][c] = 'Q';
+                col[c] = true;
+                ld[r + c] = true;
+                rd[r - c + n - 1] = true;
+                
+                solve(n, board, r + 1);
+                
+                board[r][c] = '.';
+                col[c] = false;
+                ld[r + c] = false;
+                rd[r - c + n - 1] = false;
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+
+        ld.resize(2 * n, false);
+        rd.resize(2 * n, false);
+        col.resize(n, false);
+        
+        solve(n, board, 0);
+        
+        return count;
+    }
+};
