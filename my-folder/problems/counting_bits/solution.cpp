b0f435045acc84e395f3792a4400add74a1badd3
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2022-03-01 08:36:42 +0000

    Sync LeetCode submission - Counting Bits (cpp)

---
+++ my-folder/problems/counting_bits/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> answer(n + 1);
+        
+        for(int i = 1; i <= n; i++) 
+            answer[i] = answer[i / 2] + i % 2;
+        
+        return answer;
+    }
+};
