ac07ba0a08123c72d591af7b01517b0a5a58f119
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2022-03-01 08:37:32 +0000

    Sync LeetCode submission - Counting Bits (java)

--- my-folder/problems/counting_bits/solution.java
+++ my-folder/problems/counting_bits/solution.java
@@ -1,25 +1,10 @@
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        return (int)(Math.ceil((Math.log(n) / Math.log(2)))) == 
-       (int)(Math.floor(((Math.log(n) / Math.log(2)))));
-    }
-    
     public int[] countBits(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
+        int[] answer = new int[n + 1];
         
-        if(n == 1) dp[1] = 1;
-        if(n > 1) dp[1] = dp[2] = 1;
+        for(int i = 1; i <= n; i++) 
+            answer[i] = answer[i / 2] + i % 2;
         
-        for(int i = 3; i <= n; i++) {
-            if(i % 2 != 0) {
-                dp[i] = dp[i - 1] + 1;
-            } else {
-                dp[i] = dp[i / 2];
-            }
-        }
-        
-        return dp;
+        return answer;  
     }
 }
