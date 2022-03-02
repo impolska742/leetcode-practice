76ef517baa2e1599118013818e5666c5f8feac8b
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2022-03-02 09:18:09 +0000

    Sync LeetCode submission - Pascal's Triangle II (cpp)

---
+++ my-folder/problems/pascal's_triangle_ii/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1);
+        row[0] = 1;
+        for(int i = 1; i <= rowIndex; i++) {
+            for(int j = i; j > 0; j--)
+                row[j] = row[j] + row[j - 1];
+        }
+        
+        return row;
+    }
+};
