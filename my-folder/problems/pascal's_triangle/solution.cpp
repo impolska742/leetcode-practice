9f1f58760348a5209018def2d8cf6eff6f2ea0e7
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2022-03-02 09:11:51 +0000

    Sync LeetCode submission - Pascal's Triangle (cpp)

--- my-folder/problems/pascal's_triangle/solution.cpp
+++ my-folder/problems/pascal's_triangle/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        res.push_back({1});
         
-	vector<vector<int>> Triangle(numRows);
-
-	for (int i = 0; i < numRows; i++) {
-		// Changing the size of each row
-		Triangle[i].resize(i + 1);
-		// Setting the first and the last values as 1
-		Triangle[i][0] = Triangle[i][i] = 1;
-
-		for (int j = 1; j < i; j++) {
-			Triangle[i][j] = Triangle[i - 1][j - 1] + Triangle[i - 1][j];
-		}
-	}
-
-	return Triangle;
-}
+        for(int i = 2; i <= numRows; i++) {
+            vector<int> temp(i);
+            temp[0] = 1, temp[i - 1] = 1;
+            int k = 0;
+        
+            for(int j = 1; j < i - 1 and k < i - 1; j++, k++) {
+                temp[j] = res.back()[k] + res.back()[k + 1];
+            }
+            
+            res.push_back(temp);
+        }
+        
+        return res;
+    }
 };
