b3a6416f904b30edaf5e76b2209ec1ce56c70e56
Vaibhav Bhardwaj <43564765+impolska742@users.noreply.github.com>
2022-03-03 13:54:56 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (53.73%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,17 +1,17 @@
 class Solution {
-public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        int res = 0, curr = 0;
-        int n = (int) nums.size();
-        for(int i = 1; i < n - 1; i++) {
-            if(nums[i] - nums[i - 1] == nums[i + 1] - nums[i]) {
-                curr++;
-                res += curr;
+    public int numberOfArithmeticSlices(int[] nums) {
+        int dp = 0;
+        int res = 0;
+        
+        for(int i = 1; i < nums.length - 1; i++) {
+            if(nums[i] -  nums[i - 1] == nums[i + 1] - nums[i]) {
+                dp += 1;
+                res += dp;
             } else {
-                curr = 0;
+                dp = 0;
             }
         }
         
         return res;
     }
-};
+}
