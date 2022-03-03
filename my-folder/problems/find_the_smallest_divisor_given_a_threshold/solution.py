c1a2926765a4334ff05fac32bed77f55db5d7b5b
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2022-03-03 16:55:23 +0000

    Sync LeetCode submission - Find the Smallest Divisor Given a Threshold (python3)

---
+++ my-folder/problems/find_the_smallest_divisor_given_a_threshold/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def good(m : int, nums: List[int], threshold: int) -> bool:
+            cnt = 0
+            for num in nums:
+                cnt += (num + m - 1) // m
+            
+            return cnt <= threshold
+        
+        l = 1
+        r = max(nums)
+        ans = 1
+        
+        while l <= r:
+            m = l + (r - l)//2
+            if(good(m, nums, threshold)):
+                ans = m
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return ans
+    
+    
